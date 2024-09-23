f9030a2109eef4dfe1a9cc90c4b19b2eedbeb808
SidharthMuthu0 <B00149989@mytudublin.ie>
2024-09-23 16:34:14 +0100

    Added StyleSheet

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import{View, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
 export default function App(){
 
 return(
   <View>
-   <Text>Hello, World</Text>
+   <Text style={styles.paragraph}>Hello,World</Text>
    </View>
-
 )
 }
+
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
